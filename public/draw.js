163d0bc149930d0cc5230620db6924283754d455
jfloc <120976649+jfloc@users.noreply.github.com>
2024-01-20 16:31:32 -0600

    Fixed the issue where it did not center and was not even with any aspect ratio

--- public/draw.js
+++ public/draw.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const inputLength = document.getElementById("inputLength");
   const inputWidth = document.getElementById("inputWidth");
-  const canvasContainer = document.getElementById("canvasContainer");
-  const canvas = document.getElementById("floorCanvas");
 
   inputLength.addEventListener("input ", updateCanvas);
   inputLength.addEventListener("change", updateCanvas);
@@ -10,19 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
   inputWidth.addEventListener("change", updateCanvas);
 
   function updateCanvas() {
+    const canvasContainer = document.getElementById("canvasContainer");
+    const canvas = document.getElementById("floorCanvas");
     const canvasStyle = window.getComputedStyle(canvasContainer);
+
     let lengthValue = parseFloat(inputLength.value) || 0;
     let widthValue = parseFloat(inputWidth.value) || 0;
+
+    //
     let canvasWidth = parseInt(canvasStyle.width, 10);
     let canvasHeight = parseInt(canvasStyle.height, 10);
     let size = Math.min(canvasWidth, canvasHeight);
+    console.log("width of canvas before ", canvasWidth);
+    console.log("length of canvas before ", canvasHeight);
 
-    canvasContainer.width = size;
-    canvasContainer.height = size;
     canvas.width = size;
     canvas.height = size;
 
     const ctx = canvas.getContext("2d");
+
     console.log("width of canvas is ", canvas.width);
     console.log("length of canvas is ", canvas.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -32,9 +36,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     xOffset = Math.floor(widthValue / 2);
     yOffset = Math.floor(lengthValue / 2);
+
+    if (canvasWidth > canvasHeight) {
+      aspectRatioValue = canvasHeight / canvasWidth;
+      widthValue = Math.floor(widthValue * aspectRatioValue);
+      xOffset = Math.floor(xOffset * aspectRatioValue);
+    } else if (canvasHeight > canvasWidth) {
+      aspectRatioValue = canvasWidth / canvasHeight;
+      lengthValue = Math.floor(lengthValue * (canvasWidth / canvasHeight));
+      yOffset = Math.floor(yOffset * aspectRatioValue);
+    }
+
     xValue = startX - xOffset;
     yValue = startY - yOffset;
-
     console.log("xValue is ", xValue);
     console.log("yValue is ", yValue);
     console.log("Updating canvas...");
